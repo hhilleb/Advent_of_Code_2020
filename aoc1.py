8f3550e9ceb5444692fc29f66cd359bad56585b0
Henning Hillebrandt <h_hillebrandt@gmx.de>
2020-12-02 11:04:30 +0100

    Day 1 & 2

---
+++ aoc1.py
@@ -0,0 +1,11 @@
+with open('aoc_input_1.txt') as f:
+    numbers = [int(line.rstrip()) for line in f]
+
+for x in numbers:
+    for y in numbers:
+        for z in numbers:
+            if x + y + z == 2020:
+                print(x*y*z)
+                exit()
+
+print("Nothing found...")
---
+++ aoc2.py
@@ -0,0 +1,21 @@
+import re
+
+with open('aoc_input_2.txt') as f:
+    lines = [re.split("-| |: ", line.rstrip()) for line in f]
+
+n = 0
+
+# PART 1
+# for i in lines:
+#     occ = i[3].count(i[2])
+#     if occ >= int(i[0]) and occ <= int(i[1]):
+#         n += 1
+
+# PART 2
+for i in lines:
+    a = i[3][int(i[0])-1] == i[2]
+    b = i[3][int(i[1])-1] == i[2]
+    if bool(a) != bool(b): # xor
+        n += 1
+
+print(n)
